a19595f25962a49a21de561b4cc2a3a7e1173462
bishoe01 <bishoe01@gmail.com>
2023-08-30 15:56:06 +0900

    cookie

--- dd_web/src/api/auth.ts
+++ dd_web/src/api/auth.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { BASE_URL } from ".";
-
 export interface UserInfo {
   id: string;
   pw: string;
@@ -11,6 +10,25 @@ export interface Tokens {
   refresh_token: string;
 }
 
+// export async function getLogin(userInfo: UserInfo): Promise<Tokens | null> {
+//   try {
+//     const response = await axios.get(`${BASE_URL}/user/login`, {
+//       params: userInfo,
+//     });
+
+//     if (response.status === 200 && response.data) {
+//       console.log("Login Success:", response.data.data);
+//       sessionStorage.setItem("access_token", response.data.data.access_token);
+//       sessionStorage.setItem("refresh_token", response.data.data.refresh_token);
+//       return response.data;
+//     }
+//     return null;
+//   } catch (error: unknown) {
+//     console.error("Login Error:", error);
+//     return null;
+//   }
+// }
+
 export async function getLogin(userInfo: UserInfo): Promise<Tokens | null> {
   try {
     const response = await axios.get(`${BASE_URL}/user/login`, {
@@ -19,13 +37,33 @@ export async function getLogin(userInfo: UserInfo): Promise<Tokens | null> {
 
     if (response.status === 200 && response.data) {
       console.log("Login Success:", response.data.data);
-      sessionStorage.setItem("access_token", response.data.data.access_token);
-      sessionStorage.setItem("refresh_token", response.data.data.refresh_token);
+
+      // Save tokens in cookies
+      document.cookie = `access_token=${response.data.data.access_token}; path=/`;
+      document.cookie = `refresh_token=${response.data.data.refresh_token}; path=/`;
+
       return response.data;
     }
     return null;
   } catch (error: unknown) {
     console.error("Login Error:", error);
+    return null;
+  }
+}
+
+export async function getRefresh(): Promise<Tokens | any> {
+  try {
+    const response = await axios.post(`${BASE_URL}/user/refresh`, {
+      refresh_token: sessionStorage.getItem("refresh_token"),
+    });
+
+    if (response.status === 200 && response.data) {
+      console.log("Refresh Success:", response);
+      return response.data;
+    }
+    return null;
+  } catch (error: unknown) {
+    console.error("Refresh Error:", error);
     return null;
   }
 }
--- dd_web/src/app/page.tsx
+++ dd_web/src/app/page.tsx
@@ -1,74 +1,24 @@
-"use client";
-import { Tokens, UserInfo, getLogin } from "@/api/auth";
-import React, { useEffect, useState } from "react";
+import nextCookies from "next-cookies";
 
-function LoginPage() {
-  const [userInfo, setUserInfo] = useState<UserInfo>({ id: "", pw: "" });
-  const [tokens, setTokens] = useState<Tokens | null>(null);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setUserInfo({
-      ...userInfo,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const tokens = await getLogin(userInfo);
-
-    if (tokens) {
-      setTokens(tokens);
-    } else {
-      alert("Login Failed");
-    }
-  };
-
+export default function mainPage({ accessToken, refreshToken }) {
   return (
-    <section className="p-6 py-12 flex flex-col gap-4 justify-center">
-      <div className="flex flex-col mb-4">
-        <h1 className="text-2xl font-bold">DDING DDONG</h1>
-        <span className="text-md text-gray-400">
-          Please enter your ID and Password
-        </span>
-      </div>
-      <form onSubmit={handleSubmit} className="flex flex-col my-4">
-        <div className="flex flex-col mb-2">
-          <label htmlFor="id" className="text-gray-400 text-md">
-            ID
-          </label>
-          <input
-            type="text"
-            id="id"
-            name="id"
-            className="border-b-2 px-3 py-1 mt-1 focus:outline-transparent"
-            value={userInfo.id}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex flex-col mb-2">
-          <label htmlFor="pw" className="text-gray-400 text-md">
-            Password
-          </label>
-          <input
-            type="password"
-            id="pw"
-            name="pw"
-            className="border-b-2 px-3 py-1 mt-1 focus:outline-transparent"
-            value={userInfo.pw}
-            onChange={handleChange}
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full p-2 bg-primary text-white rounded-md mt-4"
-        >
-          Log In
-        </button>
-      </form>
-    </section>
+    <div>
+      <div>mainPage</div>
+      <div>Access Token: {accessToken}</div>
+      <div>Refresh Token: {refreshToken}</div>
+    </div>
   );
 }
 
-export default LoginPage;
+export async function getServerSideProps(context) {
+  const allCookies = nextCookies(context);
+  const accessToken = allCookies["access_token"];
+  const refreshToken = allCookies["refresh_token"];
+
+  return {
+    props: {
+      accessToken,
+      refreshToken,
+    },
+  };
+}
